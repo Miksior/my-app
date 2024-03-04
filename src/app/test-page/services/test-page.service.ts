ee5176f915aafbd5ffaecef70766496fc964ab45
Miksior <morek1@op.pl>
2024-03-04 14:28:58 +0100

    Linter

--- src/app/test-page/services/test-page.service.ts
+++ src/app/test-page/services/test-page.service.ts
@@ -2,14 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 
+interface TestInterface {
+  testField: string
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class TestPageService {
   private http = inject(HttpClient)
 
-  public testGet(id: number): Observable<true>{
+  public testGet(id: number): Observable<TestInterface>{
     const url = `api/test/${id}`;
-    return this.http.get<any>(url)
+    return this.http.get<TestInterface>(url)
   }
 }
